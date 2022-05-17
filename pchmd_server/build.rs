3fb023b1ae5bbdc785156d2fdf3759aa3d98c4fc
Melvin Wang <melvin.mc.wang@gmail.com>
2022-05-17 18:01:03 -0400

    add libsensors rust bindings

--- pchmd_server/build.rs
+++ pchmd_server/build.rs
@@ -1,10 +1,29 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
+    // TODO: put all rerun-if statements at beginning.
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // TODO: put all pre-requisite checks before other build steps
+    // TODO: convert pre-req checks to use pkg-config/cmake etc
+    // TODO: commonize/split up os-specific build steps
+
+    generate_capnproto_files();
+    generate_libsensors_bindings();
+    generate_librehardwaremonitor_bindings();
+}
+
+
+fn generate_capnproto_files() {
+    println!("cargo:rerun-if-changed=schema/pchmd.capnp");
+
     std::process::Command::new("capnp")
         .args([
             "--version",
-    ])
-    .output()
-    .expect("`capnp` binary not found! Try looking for and installing the `capnproto` package in your package manager(ex. sudo apt install capnproto).");
+        ])
+        .output()
+        .expect("`capnp` binary not found! Try looking for and installing the `capnproto` package in your package manager(ex. sudo apt install capnproto).");
 
     capnpc::CompilerCommand::new()
         .src_prefix("schema")
@@ -12,3 +31,25 @@ fn main() {
         .run()
         .expect("`capnpc` failed to compile rust bindings for schemas!");
 }
+
+fn generate_libsensors_bindings() {
+    // TODO: check if libsensors is available (aka whether this system can poll libsensors)
+    // TODO: check if libsensors-dev is available
+
+    println!("cargo:rustc-link-lib=sensors");
+    println!("cargo:rerun-if-changed=src/libsensors-wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("src/libsensors-wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate rust bindings for libsensors");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("libsensors-bindings.rs"))
+        .expect("Couldn't write bindings for libsensors!");
+}
+
+fn generate_librehardwaremonitor_bindings() {}
--- pchmd_server/src/lib.rs
+++ pchmd_server/src/lib.rs
@@ -1,3 +1,12 @@
 pub mod pchmd_capnp {
     include!(concat!(env!("OUT_DIR"), "/pchmd_capnp.rs"));
 }
+
+// TODO: Add libsensors integration tests (ex. sanity tests)
+mod lib_sensors {
+    #![allow(non_camel_case_types)]
+    #![allow(non_upper_case_globals)]
+    #![allow(non_snake_case)]
+    #![allow(dead_code)]
+    include!(concat!(env!("OUT_DIR"), "/libsensors-bindings.rs"));
+}
