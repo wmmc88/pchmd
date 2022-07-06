0bb2ae632ead51223d2b416d3491a2304bfb6a81
wmmc88 <melvin.mc.wang@gmail.com>
2022-07-05 20:03:32 -0400

    add scrolling grid view(variable height) with placeholder tiles

--- pchmd_gui/lib/main.dart
+++ pchmd_gui/lib/main.dart
@@ -1,114 +1,111 @@
+import 'dart:math';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PCHMDGUIApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PCHMDGUIApp extends StatelessWidget {
+  const PCHMDGUIApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PCHMD GUI',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My Computer Sensors'),
+          // TODO: parse from incoming data's computer name
+        ),
+        body: const Center(child: DataSpace()));
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class DataSpace extends StatefulWidget {
+  const DataSpace({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  State<DataSpace> createState() => _DataSpaceState();
+}
+
+class _DataSpaceState extends State<DataSpace> {
+  final heightBase = 100;
+  final heightOffset = 30;
+  final numItems = 15;
+
+  late final Random random;
+  late final int randomColorOffset;
+  late final List<double> heights;
+
+  @override
+  void initState() {
+    super.initState();
+    random = Random();
+    randomColorOffset = random.nextInt(Colors.primaries.length);
+    heights = List.generate(
+        numItems,
+        (index) =>
+            (random.nextInt(2 * heightOffset) + heightBase - heightOffset)
+                .toDouble(),
+        growable: false);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return MasonryGridView.count(
+      crossAxisCount: 2,
+      padding: const EdgeInsets.all(2.5),
+      mainAxisSpacing: 5,
+      crossAxisSpacing: 5,
+      itemBuilder: (context, index) {
+        return DataSpaceContainer(
+            backgroundColor: Colors.primaries[
+                (index + randomColorOffset) % Colors.primaries.length],
+            verticalPadding: heights[index],
+            child: Text(index.toString()));
+      },
+      itemCount: numItems,
     );
+  }
+}
+
+class DataSpaceContainer extends StatefulWidget {
+  final MaterialColor backgroundColor;
+  final Widget? child;
+  final double verticalPadding;
+
+  const DataSpaceContainer(
+      {Key? key,
+      this.child,
+      required this.backgroundColor,
+      this.verticalPadding = 100})
+      : super(key: key);
+
+  @override
+  State<DataSpaceContainer> createState() => _DataSpaceContainerState();
+}
+
+class _DataSpaceContainerState extends State<DataSpaceContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(
+            vertical: widget.verticalPadding, horizontal: 15),
+        color: widget.backgroundColor,
+        child: Center(child: widget.child));
   }
 }
--- pchmd_gui/test/widget_test.dart
+++ pchmd_gui/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pchmd_gui/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PCHMDGUIApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
